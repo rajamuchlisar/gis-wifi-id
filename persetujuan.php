81d4c6c19a58012d1878a4f686d140636a3824e2
rajamuchlisar <57666835+rajamuchlisar@users.noreply.github.com>
2019-11-12 19:20:07 +0700

    Create persetujuan.php

---
+++ persetujuan.php
@@ -0,0 +1,5 @@
+tolong buat form persetujuan, dimana saat user menambah data, maka secara automatis data tersebut berstatus (nonaktif).
+form persetujuan ini hanya bisa terbuka dengan login sebgaai admin. dan hanya akun berlevel admin yang dapat merubah status ( nonaktif) ke (aktif).
+
+dan di index.php tolong buat hanya data yang berstatus aktif saja yang hanya tampil di index. sedangkan yang status non aktif tidak tampil.
+
